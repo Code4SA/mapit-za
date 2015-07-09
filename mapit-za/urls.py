8cdf1ee9d4b1329c73ab2e594e36f35af2afba6a
xybrnet <damian@code4sa.org>
2015-07-09 15:31:40 +0200

    Fix urls

--- mapit-za/urls.py
+++ mapit-za/urls.py
@@ -3,8 +3,7 @@ from django.contrib import admin
 from django.views.generic.base import RedirectView
 
 urlpatterns = patterns('',
-    url(r'^$', RedirectView.as_view(url='/mapit/'), name='home'),
-    url(r'^mapit/', include('mapit.urls')),
+    url(r'', include('mapit.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
