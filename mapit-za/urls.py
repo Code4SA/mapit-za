d59ba982c74177ea4d39908b74b55dfeedd148f1
xybrnet <damian@code4sa.org>
2015-07-09 12:51:08 +0200

    Redirect root url to mapit/

--- mapit-za/urls.py
+++ mapit-za/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 urlpatterns = patterns('',
-    url(r'^$', 'mapit-za.views.home', name='home'),
+    url(r'^$', RedirectView.as_view(url='/mapit/'), name='home'),
     url(r'^mapit/', include('mapit.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
