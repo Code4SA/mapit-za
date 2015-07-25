5af98ec2a7279f97bc5757eeb293d07de33e35ee
Greg Kempe <greg@kempe.net>
2015-07-25 09:26:47 +0200

    Remove CSRF protection

--- mapit-za/settings.py
+++ mapit-za/settings.py
@@ -54,7 +54,6 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
