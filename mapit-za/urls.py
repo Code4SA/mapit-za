c6e71d08268292979053fe373a93ea6eea1dff6c
Greg Kempe <greg@kempe.net>
2015-07-26 15:54:47 +0200

    Stop google from indexing the site.

--- mapit-za/urls.py
+++ mapit-za/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.views.generic.base import RedirectView
+from django.views.generic import TemplateView
+
 
 urlpatterns = patterns('',
     url(r'', include('mapit.urls')),
+    url(r'^robots.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
